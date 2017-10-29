a0291995303fd79aed5c20ad2b81df4e2eb753b9
Stephan-kashkarov <32626048+Stephan-kashkarov@users.noreply.github.com>
2017-10-29 11:46:02 +1100

    renamed a file

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 import time
 import random
 import math
@@ -7,22 +7,40 @@ from setup import *
 from functions import *
 import pytmx
 
-class main_map():
-	def __init__(self, filename):
-		mp = pytmx.load_pygame(filename, pixelalpha = True)
-		self.width = tm.width * tm.tilewidth
-		self.height = tm.height * tm.tileheight
-		self.tmxdata = tm
+class Renderer(object):  #https://github.com/justinmeister/PyTMX-Examples/blob/master/Making%20a%20map%20surface/tilerender.py
+    """
+    This object renders tile maps from Tiled
+    """
+    def __init__(self, filename):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.size = tm.width * tm.tilewidth, tm.height * tm.tileheight
+        self.tmx_data = tm
 
-	def render(self,surface):
-		ti = self.tmxdata.get_tile_image_by_gid
-		for layer in self.tmxdata.visible_Layers:
-			if isinstance(layer,pytmx.TiledTileLayer):
-				for x, y, gid, in layer:
-					tile = ti(gid)
-					if tile:
-						gameDisplay.blit(tile, x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight)
-	def make_map(self):
-		temp_surf = pygame.gameDisplay((self.height, self.width))
-		self.render(temp_surf)
-		return temp_surf
+    def render(self, surface):
+
+        tw = self.tmx_data.tilewidth
+        th = self.tmx_data.tileheight
+        gt = self.tmx_data.getTileImageByGid
+
+        if self.tmx_data.background_color:
+            surface.fill(self.tmx_data.background_color)
+
+        for layer in self.tmx_data.visibleLayers:
+            if isinstance(layer, pytmx.TiledLayer):
+                for x, y, gid in layer:
+                    tile = gt(gid)
+                    if tile:
+                        surface.blit(tile, (x * tw, y * th))
+
+            elif isinstance(layer, pytmx.TiledObjectGroup):
+                pass
+
+            elif isinstance(layer, pytmx.TiledImageLayer):
+                image = gt(layer.gid)
+                if image:
+                    surface.blit(image, (0, 0))
+
+    def make_map(self):
+        temp_surface = pg.Surface(self.size)
+        self.render(temp_surface)
+        return temp_surface
