a2d9c17fec135b3bd64384dda0fcc09a52ac72dc
Stephan-kashkarov <32626048+Stephan-kashkarov@users.noreply.github.com>
2017-11-03 13:53:56 +1100

    re-reorganised game

--- re organised/Main.py
+++ re organised/Main.py
@@ -1,67 +1,49 @@
-#main
 import pygame as pg
-import random
-from temp_settings import *
+from sprites import *
+from settings import *
+from game_map import *
 
-class Game:
-	def __init__(self):
-		#initializer
+class game():
+	"""docstring for screen"""
+	def __init__(self, display_height, display_width, fps):
 		pg.init()
 		pg.mixer.init()
-		self.running = True
+		self.display_height = display_height
+		self.display_width = display_width
+		self.fps = fps
 		self.screen = pg.display.set_mode((width,height))
-		pg.set_caption(title)
+		pg.display.set_caption(title)
 		self.clock = pg.time.Clock()
+		pg.key.set_repeat(100, 50)
+		self.load_data()
 
-
-	def game_init(self):
-		#initiaizes game
-		self.all_sprites = pg.sprites.Group()
-		self.playing = True
-		self.run()
-
-	def run(self):
-		#game loop
-		self.playing = True
-		while self.playing:
-			self.clock.tick(fps)
-			self.events()
-			self.draw()
-
-	def update(self):
-		#game loop update
-		self.all_sprites()
-
-	def events(self):
-		#game loop events
-		mouse_pos = pg.mouse.get_pos() # Get the mouse position
-		mouse_click = pg.mouse.get_pressed() # Get the mouse button state
-		for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-						pygame.quit()
-						quit()
-				if event.type == pygame.KEYDOWN:
-					pass
-				elif event.type == pygame.KEYUP:
-					pass
-
-	def draw(self):
-		#game loop draw
-		self.screen.fill(black)
-		self.all_sprites.draw(sreen)
-		pg.display.flip()
-
-
-	def start(self):
+	def load_data(self):
 		pass
 
-	def end(self):
+	def intro(self):
 		pass
 
-g = Game()
-g.start()
-while g.running:
-	g.new()
-	g.end()
-pygmae.quit()
-quit()
+	def menu(self):
+		pass
+
+	def hq(self):
+		pass
+
+	def patrol(self):
+		pass
+
+	def battle(self):
+		pass
+
+
+
+
+
+#game.intro()
+#game.menu()
+#while game:
+#	game.hq()
+#	game.patrol()
+#		game.battle()
+#game.boss()
+#quit()
--- re organised/Settings.py
+++ re organised/Settings.py
@@ -1,16 +1,7 @@
-#game options
-title = "Template"
-width = 1280
-height = 1080
-fps = 120
+#PLAYER STATS
+PLAYER_SPEED = 100
 
-#COLOURS
-black = (0,0,0) # the Black colour in RBG
-grey = (100,100,100) # the Grey colour in RBG
-d_grey = (50,50,50) # the Dark Grey colour in RBG
-white = (255,255,255) # the White colour in RBG
-red = (255,0,0) # the Red colour in RBG
-d_red = (200,0,0) # the Dark Red colour in RBG
-green = (0,255,0) # the Green colour in RBG
-d_green = (0,200,0) # the Dark green colour in RBG
-blue = (0,0,255) # the Blue colour in RBG
+#SCREEN STATS
+display_width = 1280
+display_height = 720
+title = "plane_game"
